f108e5f5722aea09e92712fe594716770fdcb566
tjbklx1 <wolfs_2004@aliyun.com>
2016-08-22 22:25:54 +0800

    add ant demo

---
+++ antdemo/src/ant1/HelloAnt.java
@@ -0,0 +1,14 @@
+package ant1;
+
+class HelloAnt {
+
+	/**
+	 * @param args
+	 */
+	private static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello ant");
+		System.out.println(0000);
+	}
+
+}
---
+++ antdemo/src/ant1/NewFrame.java
@@ -0,0 +1,14 @@
+package ant1;
+
+import javax.swing.JFrame;
+
+public class NewFrame {
+	public static void main(String[] args) {
+		//JFrame frame =new JFrame();
+		
+		
+		
+		
+	}
+
+}
